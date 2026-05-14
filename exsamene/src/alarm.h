423ac0c749060cfb30051c89a77287ff30b25444
Mathias Otterlei Vatnehol <mathiasov@uia.no>
2026-05-14 20:12:33 +0200

    Add alarm and temperature humidity modules

---
+++ exsamene/src/alarm.h
@@ -0,0 +1,21 @@
+#ifndef ALARM_H
+#define ALARM_H
+
+// ================================
+// Alarm thread
+// ================================
+void alarm_thread_entry(void *arg1, void *arg2, void *arg3);
+
+// ================================
+// TODO: kall alarm_snooze() når snooze-knappen trykkes
+// TODO: kall alarm_mute() når mute-knappen trykkes
+// TODO: kall alarm_toggle_enabled() når enable-knappen trykkes
+// ================================
+void alarm_snooze(void);
+void alarm_mute(void);
+void alarm_toggle_enabled(void);
+void alarm_get_status_text(char *buffer, int buffer_size);
+
+
+#endif
+
